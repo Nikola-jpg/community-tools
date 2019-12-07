ba418bd53d3e50b3fdf49a6dd2ff3e92cdb57d3a
ponomaryova-yulia <57496830+ponomaryova-yulia@users.noreply.github.com>
2019-12-07 20:00:55 +0200

    Update SlackServiceTest.java

--- src/test/java/com/community/tools/SlackServiceTest.java
+++ src/test/java/com/community/tools/SlackServiceTest.java
@@ -1,11 +1,17 @@
 package com.community.tools;
 
+import com.github.seratch.jslack.Slack;
 import com.github.seratch.jslack.api.methods.SlackApiException;
+import com.github.seratch.jslack.api.methods.response.chat.ChatDeleteResponse;
+import com.github.seratch.jslack.api.methods.response.chat.ChatPostMessageResponse;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 
-import javax.websocket.DeploymentException;
 import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SlackServiceTest {
   
