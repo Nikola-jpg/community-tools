09692801776ab08369298ee31cf7584b69b71ecf
ponomaryova-yulia <57496830+ponomaryova-yulia@users.noreply.github.com>
2019-12-07 19:59:59 +0200

    Update SlackService.java

--- src/main/java/com/community/tools/SlackService.java
+++ src/main/java/com/community/tools/SlackService.java
@@ -3,8 +3,6 @@ package com.community.tools;
 import com.github.seratch.jslack.*;
 import com.github.seratch.jslack.api.methods.SlackApiException;
 import com.github.seratch.jslack.api.methods.response.chat.ChatPostMessageResponse;
-import com.github.seratch.jslack.api.methods.response.im.ImListResponse;
-import com.github.seratch.jslack.api.methods.response.users.UsersListResponse;
 import com.github.seratch.jslack.api.model.Im;
 import com.github.seratch.jslack.api.model.User;
 import com.github.seratch.jslack.api.rtm.*;
@@ -13,11 +11,11 @@ import com.github.seratch.jslack.api.rtm.message.Typing;
 import com.google.gson.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import javax.websocket.DeploymentException;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
 
 @Service("slack")
 public class SlackService {
