52ec39f3e8eb3888a34ef08e6580ba9c84a87ff9
Dmitry Makarenko <40339618+dafuct@users.noreply.github.com>
2019-12-17 14:44:16 +0200

    Github service
    * Create Spring boot, HelloController: method return json
    
    * add dependency github Api
    
    * add Java 8 Version in Maven
    
    * add Config for read application.properties and service for get pull request
    
    * change in methods getPullRequest() email - on title
    and create GetMapping in Controller
    
    * create package controller and move GitHubController
    
    * remove class Configuration, make @Value for read properties and IOException -> throw RuntimeException

---
+++ src/main/java/com/community/tools/controller/GitHubController.java
@@ -0,0 +1,37 @@
+package com.community.tools.controller;
+
+import static org.springframework.http.ResponseEntity.*;
+
+import com.community.tools.service.GitHubService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitHubController {
+
+  @Autowired
+  private GitHubService service;
+
+  @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<List<String>> getHelloInJson() {
+    List<String> list = new ArrayList<>();
+    list.add("Hello");
+    list.add("World");
+    return ok().body(list);
+  }
+
+  @GetMapping(value = "/pull_request/{state}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<List<Map<String, String>>> getPullRequests(@PathVariable boolean state) {
+    Map<String, String> userPullRequest = service.getPullRequests(state);
+    List<Map<String, String>> list = new ArrayList<>();
+    list.add(userPullRequest);
+    return ok().body(list);
+  }
+}
---
+++ src/main/java/com/community/tools/service/GitHubService.java
@@ -0,0 +1,56 @@
+package com.community.tools.service;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GitHubService {
+
+  @Value("${token}")
+  private String token;
+
+  @Value("${repository}")
+  private String nameRepository;
+
+  public Map<String, String> getPullRequests(boolean statePullRequest) {
+    Map<String, String> listUsers = new HashMap<>();
+    try {
+      GitHub gitHub = getGitHubConnection();
+      GHRepository repository = gitHub.getRepository(nameRepository);
+
+      List<GHPullRequest> pullRequests;
+      if (!statePullRequest) {
+        pullRequests = repository.getPullRequests(GHIssueState.CLOSED);
+      } else {
+        pullRequests = repository.getPullRequests(GHIssueState.OPEN);
+      }
+
+      for (GHPullRequest repo : pullRequests) {
+        String login = repo.getUser().getLogin();
+        String title = repo.getTitle();
+        listUsers.put(login, title);
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return listUsers;
+  }
+
+  private GitHub getGitHubConnection() {
+    GitHub gitHub;
+    try {
+      gitHub = GitHub.connectUsingOAuth(token);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return gitHub;
+  }
+}
