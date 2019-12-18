91f8224b157191e9fe9317e8326922d2c43b190f
ponomaryova-yulia <57496830+ponomaryova-yulia@users.noreply.github.com>
2019-12-18 17:14:12 +0200

    Update SlackServiceTest.java

--- src/test/java/com/community/tools/SlackServiceTest.java
+++ src/test/java/com/community/tools/SlackServiceTest.java
@@ -14,32 +14,12 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SlackServiceTest {
-  
-  @Value("${slack.token}")
-  private String token;
 
   private static SlackService slackService;
 
   @BeforeAll
   public static void initSlackService() {
     slackService = new SlackService();
-  }
-
-  @Test
-  void sendPrivateMessage() throws IOException, SlackApiException {
-    //setup
-    String actualInputData_username = "Slackbot";
-    String actualInputData_messageText = "test message";
-    //execute
-    ChatPostMessageResponse response =
-            slackService.sendPrivateMessage(actualInputData_username, actualInputData_messageText);
-    Slack slack = Slack.getInstance();
-    ChatDeleteResponse deleteResponse =
-            slack.methods(token).chatDelete(req ->
-                    req.channel(response.getChannel()).ts(response.getTs()));
-    boolean actualData = deleteResponse.isOk();
-    //verify
-    assertTrue(actualData);
   }
 
   @Test
