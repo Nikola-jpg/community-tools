075f0c0baa050835b065b2a891ca42f564a0fec9
Dmitry <dafuct@gmail.com>
2019-12-19 15:40:00 +0200

    add @nonArgConstructor in Event

--- src/main/java/com/community/tools/model/Event.java
+++ src/main/java/com/community/tools/model/Event.java
@@ -3,13 +3,16 @@ package com.community.tools.model;
 import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Event {
 
   private Date createdAt;
   private String actorLogin;
   private String type;
   private String state;
+
 }
