08c9721cc569a7c03f1af4bf7837e90e4a06f91d
Roman Zagorulko <rr.zagorulko@gmail.com>
2020-04-27 15:33:41 +0300

    refactor stateMachine from jdbc to jpa

--- src/main/java/com/community/tools/util/statemachie/StateMachinePersister.java
+++ src/main/java/com/community/tools/util/statemachie/StateMachinePersister.java
@@ -1,14 +1,13 @@
 package com.community.tools.util.statemachie;
 
+import com.community.tools.util.statemachie.jpa.StateEntity;
+import com.community.tools.util.statemachie.jpa.StateMachineRepository;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.util.Arrays;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.statemachine.StateMachineContext;
 import org.springframework.statemachine.StateMachinePersist;
 import org.springframework.statemachine.kryo.StateMachineContextSerializer;
@@ -17,14 +16,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class StateMachinePersister implements
     StateMachinePersist<State, Event, String> {
-
-
-  @Value("${DB_URL}")
-  private String url;
-  @Value("${spring.datasource.username}")
-  private String username;
-  @Value("${spring.datasource.password}")
-  private String password;
+  @Autowired
+  private StateMachineRepository stateMachineRepository;
 
   private static final ThreadLocal<Kryo> kryoThreadLocal = ThreadLocal.withInitial(() -> {
     Kryo kryo = new Kryo();
@@ -36,40 +29,18 @@ public class StateMachinePersister implements
   @Override
   public void write(StateMachineContext<State, Event> context, String userID) {
     byte[] data = serialize(context);
-    SingleConnectionDataSource connect = new SingleConnectionDataSource();
-    connect.setUrl(url);
-    connect.setUsername(username);
-    connect.setPassword(password);
-    JdbcTemplate jdbcTemplate = new JdbcTemplate(connect);
-    jdbcTemplate.update(
-        "UPDATE public.\"StateMachine\" SET state_machine='" + Arrays.toString(data)
-            + "'  WHERE context ='" + userID + "';");
+    stateMachineRepository.save(new StateEntity(userID,data));
   }
 
   @Override
   public StateMachineContext<State, Event> read(String s) {
-    SingleConnectionDataSource connect = new SingleConnectionDataSource();
-    connect.setUrl(url);
-    connect.setUsername(username);
-    connect.setPassword(password);
-    JdbcTemplate jdbcTemplate = new JdbcTemplate(connect);
 
-    String state = jdbcTemplate.queryForObject(
-        "SELECT state_machine FROM public.\"StateMachine\" WHERE context = '" + s + "';",
-        String.class);
-    byte[] arr = getBytes(state);
+    StateEntity stateEntity = stateMachineRepository.findByUserID(s).get();
+    byte[] arr = stateEntity.getStateMachine();
 
     return deserialize(arr);
   }
 
-  private byte[] getBytes(String state) {
-    String[] s = state.replaceAll("[^0-9 \\-]", "").split(" ");
-    byte[] bytes = new byte[s.length];
-    for (int i = 0; i < s.length; i++) {
-      bytes[i] = Byte.parseByte(s[i]);
-    }
-    return bytes;
-  }
 
   private byte[] serialize(StateMachineContext<State, Event> context) {
     Kryo kryo = kryoThreadLocal.get();
