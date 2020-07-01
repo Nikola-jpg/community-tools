ce08878f6df2c0cebeaad2d5e0c295de62bcedcb
Roman Zagorulko <rr.zagorulko@gmail.com>
2020-07-01 09:12:18 +0300

    refactor make state actions usable

--- src/main/java/com/community/tools/util/statemachie/actions/GetTheFirstTaskAction.java
+++ src/main/java/com/community/tools/util/statemachie/actions/GetTheFirstTaskAction.java
@@ -1,19 +1,31 @@
 package com.community.tools.util.statemachie.actions;
 
+import com.community.tools.service.slack.SlackService;
 import com.community.tools.util.statemachie.Event;
 import com.community.tools.util.statemachie.State;
+import com.github.seratch.jslack.api.methods.SlackApiException;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
 
-import java.util.logging.Logger;
-
-
+@Component
 public class GetTheFirstTaskAction implements Action<State, Event> {
-    private static Logger logger = Logger.getLogger(AgreeLicenseAction.class.getName());
 
-    @Override
-    public void execute(final StateContext<State, Event> context) {
-        // TODO: 22.04.2020 add logic to do smthng when user get the first task
-        logger.info("User got the first task");
+  @Value("${getFirstTask}")
+  private String getFirstTask;
+  @Autowired
+  private SlackService slackService;
+
+  @Override
+  public void execute(final StateContext<State, Event> context) {
+    String user = context.getExtendedState().getVariables().get("id").toString();
+    try {
+      slackService.sendBlocksMessage(user, getFirstTask);
+    } catch (IOException | SlackApiException e) {
+      throw new RuntimeException(e);
     }
+  }
 }
