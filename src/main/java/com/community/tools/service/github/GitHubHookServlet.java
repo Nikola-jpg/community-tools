9aaef568d9b1da0da012745658064710fa15d364
Roman Zagorulko <rr.zagorulko@gmail.com>
2020-07-16 15:00:28 +0300

    Add sending message for ew pull request

--- src/main/java/com/community/tools/service/github/GitHubHookServlet.java
+++ src/main/java/com/community/tools/service/github/GitHubHookServlet.java
@@ -1,6 +1,8 @@
 package com.community.tools.service.github;
 
+import com.community.tools.service.slack.SlackService;
 import com.community.tools.util.GithubAuthChecker;
+import com.github.seratch.jslack.api.methods.SlackApiException;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -8,8 +10,8 @@ import java.util.Date;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import lombok.SneakyThrows;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
@@ -26,6 +28,8 @@ public class GitHubHookServlet extends HttpServlet {
   private String password;
   @Value("${GITHUB_SECRET_TOKEN}")
   private String secret;
+  @Autowired
+  private SlackService service;
 
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -48,11 +52,19 @@ public class GitHubHookServlet extends HttpServlet {
         connect.setPassword(password);
         JdbcTemplate jdbcTemplate = new JdbcTemplate(connect);
 
+        if (json.get("action").toString().equals("opened")) {
+          JSONObject pull = json.getJSONObject("pull_request");
+          String user = pull.getJSONObject("user").getString("login");
+          String url = pull.getJSONObject("_links").getJSONObject("html").getString("href");
+          service
+              .sendMessageToChat("test", "User" + user + " create a pull request \n url: " + url);
+        }
+
         jdbcTemplate.update(
             "INSERT INTO public.\"GitHookData\" (time, jsonb_data) VALUES ('" + new Date() + "','"
                 + json + "'::jsonb);");
       }
-    } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+    } catch (NoSuchAlgorithmException | InvalidKeyException | SlackApiException e) {
       throw new RuntimeException(e);
     }
 
