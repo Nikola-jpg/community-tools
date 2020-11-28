be234d3bfec509ebe6d654a3ef06fbc60da1cec0
marvintik <64403964+marvintik@users.noreply.github.com>
2020-11-28 14:42:32 +0200

    Update PointsTaskService.java

--- src/main/java/com/community/tools/service/PointsTaskService.java
+++ src/main/java/com/community/tools/service/PointsTaskService.java
@@ -15,13 +15,15 @@ import javax.persistence.EntityNotFoundException;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PointsTaskService {
 
   @Value("${abilityReviewMessage}")
   private String abilityReviewMessage;
 
-  @Value("${zeroPointsMessagee}")
+  @Value("${zeroPointsMessage}")
   private String zeroPointsMessage;
 
   @Value("#{${pointsForTask}}")
