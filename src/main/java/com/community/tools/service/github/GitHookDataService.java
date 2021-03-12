91a03c9f7be498c18905b8b6373962ee9797692e
WyPik <wypik28@gmail.com>
2021-03-12 13:40:49 +0300

    fix database connection

--- src/main/java/com/community/tools/service/github/GitHookDataService.java
+++ src/main/java/com/community/tools/service/github/GitHookDataService.java
@@ -2,8 +2,10 @@ package com.community.tools.service.github;
 
 import java.sql.SQLException;
 import java.util.Date;
+import javax.sql.DataSource;
 import org.json.JSONObject;
 import org.postgresql.util.PGobject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
@@ -12,24 +14,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GitHookDataService {
-  @Value("${spring.datasource.url}")
-  private String url;
-  @Value("${spring.datasource.username}")
-  private String username;
-  @Value("${spring.datasource.password}")
-  private String password;
+
   private final String sql = "INSERT INTO public.\"GitHookData\" (time, jsonb_data) VALUES (? , ?)";
+
+  private JdbcTemplate connection;
+
+  @Autowired
+  public void initConnection(DataSource dataSource){
+    this.connection = new JdbcTemplate(dataSource);
+  }
 
   /**
    * Save JSON from GitHub to GitHookData table into Database.
    * @param json Json from GitHub
    */
   public void saveDataIntoDB(JSONObject json) {
-    SingleConnectionDataSource connect = new SingleConnectionDataSource();
-    connect.setUrl(url);
-    connect.setUsername(username);
-    connect.setPassword(password);
-    JdbcTemplate jdbcTemplate = new JdbcTemplate(connect);
     Date date = new Date();
     PGobject out = new PGobject();
     out.setType("json");
@@ -38,7 +37,6 @@ public class GitHookDataService {
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
-    jdbcTemplate.update(sql, date, out);
-    connect.destroy();
+    connection.update(sql, date, out);
   }
 }
