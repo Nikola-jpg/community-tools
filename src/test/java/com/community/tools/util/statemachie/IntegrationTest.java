25c2368ba2726aa85811748844fe71b395bd440f
Libenko Ilya <libenkouriy@gmail.com>
2021-05-17 01:31:12 +0300

    [integration_test] add tests for all actions

--- src/test/java/com/community/tools/util/statemachie/IntegrationTest.java
+++ src/test/java/com/community/tools/util/statemachie/IntegrationTest.java
@@ -1,6 +1,16 @@
 package com.community.tools.util.statemachie;
 
+import static com.community.tools.util.statemachie.State.ADDED_GIT;
+import static com.community.tools.util.statemachie.State.AGREED_LICENSE;
+import static com.community.tools.util.statemachie.State.CHECK_FOR_NEW_TASK;
+import static com.community.tools.util.statemachie.State.CHECK_LOGIN;
+import static com.community.tools.util.statemachie.State.FIRST_QUESTION;
+import static com.community.tools.util.statemachie.State.GOT_THE_FIRST_TASK;
+import static com.community.tools.util.statemachie.State.INFORMATION_CHANNELS;
+import static com.community.tools.util.statemachie.State.SECOND_QUESTION;
+import static com.community.tools.util.statemachie.State.THIRD_QUESTION;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -17,29 +27,60 @@ import java.util.HashSet;
 import java.util.Set;
 
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GHTeam;
 import org.kohsuke.github.GHUser;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.statemachine.ExtendedState;
 import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class IntegrationTest {
 
-  @Value("Oops, I'm sorry, but I don't have an answer to your request.")
-  private String defaultMessage;
+  private static final String USER_ID = "U01QY6GRZ0X";
+  private static final String USER_NAME = "Some User";
 
   @Value("${firstQuestion}")
   private String firstQuestion;
+
+  @Value("${secondQuestion}")
+  private String secondQuestion;
+
+  @Value("${thirdQuestion}")
+  private String thirdQuestion;
+
+  @Value("${messageAboutSeveralInfoChannel}")
+  private String messageAboutSeveralInfoChannel;
+
+  @Value("${addGitName}")
+  private String addGitName;
+
+  @Value("${askAboutProfile}")
+  private String askAboutProfile;
+
+  @Value("${congratsAvailableNick}")
+  private String congratsAvailableNick;
+
+  @Value("${getFirstTask}")
+  private String getFirstTask;
+
+  @Value("${answeredNoDuringVerification}")
+  private String answeredNoDuringVerification;
+
+  @Value("${lastTask}")
+  private String lastTask;
 
   @Autowired
   private StateMachineService stateMachineService;
@@ -65,27 +106,244 @@ class IntegrationTest {
   @Mock
   private GHRepository ghRepository;
 
+  @Captor
+  private ArgumentCaptor<String> firstArg;
+
+  @Captor
+  private ArgumentCaptor<String> secondArg;
+
+  private ExtendedState extendedState;
+
+  private StateMachine<State, Event> machine;
+
+  @SneakyThrows
+  @BeforeEach
+  void setUp() {
+    machine = stateMachineService
+        .restoreMachine(USER_ID);
+    machine.getExtendedState().getVariables()
+        .put("gitNick", USER_NAME);
+    machine.getExtendedState().getVariables()
+        .put("id", USER_ID);
+  }
+
   @SneakyThrows
   @Test
   void firstQuestionActionTest() {
-    String id = "U01QY6GRZ0X";
     User stateEntity = new User();
-    stateEntity.setUserID(id);
+    stateEntity.setUserID(USER_ID);
     stateMachineRepository.save(stateEntity);
-    stateMachineService.persistMachineForNewUser(id);
-    StateMachine<State, Event> machine = stateMachineService
-        .restoreMachine(id);
+    stateMachineService.persistMachineForNewUser(USER_ID);
 
-    when(slackService.getUserById(id)).thenReturn("Some User");
-    when(slackService.sendBlocksMessage("Some User", firstQuestion)).thenReturn("");
+    when(slackService.getUserById(USER_ID)).thenReturn(USER_NAME);
+    when(slackService.sendBlocksMessage(USER_NAME, firstQuestion)).thenReturn("");
 
     machine.sendEvent(Event.QUESTION_FIRST);
 
-    ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-    ArgumentCaptor<String> captor2 = ArgumentCaptor.forClass(String.class);
+    verify(slackService).sendBlocksMessage(firstArg.capture(), secondArg.capture());
+    assertEquals(USER_NAME, firstArg.getValue());
+    assertEquals(firstQuestion, secondArg.getValue());
+  }
 
-    verify(slackService).sendBlocksMessage(captor.capture(), captor2.capture());
-    assertEquals("Some User", captor.getValue());
-    assertEquals(firstQuestion, captor2.getValue());
+  @SneakyThrows
+  @Test
+  void secondQuestionActionTest() {
+    machine.getStateMachineAccessor().doWithAllRegions(access -> access
+        .resetStateMachine(new DefaultStateMachineContext<>(FIRST_QUESTION,
+            null, null, extendedState)));
+
+    when(slackService.getUserById(USER_ID)).thenReturn(USER_NAME);
+    when(slackService.sendBlocksMessage(USER_NAME, secondQuestion)).thenReturn("");
+
+    machine.sendEvent(Event.QUESTION_SECOND);
+
+    verify(slackService).sendBlocksMessage(firstArg.capture(), secondArg.capture());
+    assertEquals(USER_NAME, firstArg.getValue());
+    assertEquals(secondQuestion, secondArg.getValue());
+  }
+
+  @SneakyThrows
+  @Test
+  void thirdQuestionActionTest() {
+    machine.getStateMachineAccessor().doWithAllRegions(access -> access
+        .resetStateMachine(new DefaultStateMachineContext<>(SECOND_QUESTION,
+            null, null, extendedState)));
+
+    when(slackService.getUserById(USER_ID)).thenReturn(USER_NAME);
+    when(slackService.sendBlocksMessage(USER_NAME, thirdQuestion)).thenReturn("");
+
+    machine.sendEvent(Event.QUESTION_THIRD);
+
+    verify(slackService).sendBlocksMessage(firstArg.capture(), secondArg.capture());
+    assertEquals(USER_NAME, firstArg.getValue());
+    assertEquals(thirdQuestion, secondArg.getValue());
+  }
+
+  @SneakyThrows
+  @Test
+  void channelInformationActionTest() {
+    machine.getStateMachineAccessor().doWithAllRegions(access -> access
+        .resetStateMachine(new DefaultStateMachineContext<>(THIRD_QUESTION,
+            null, null, extendedState)));
+
+    when(slackService.getUserById(USER_ID)).thenReturn(USER_NAME);
+    when(slackService.sendBlocksMessage(USER_NAME, messageAboutSeveralInfoChannel)).thenReturn("");
+
+    machine.sendEvent(Event.CHANNELS_INFORMATION);
+
+    verify(slackService).sendBlocksMessage(firstArg.capture(), secondArg.capture());
+    assertEquals(USER_NAME, firstArg.getValue());
+    assertEquals(messageAboutSeveralInfoChannel, secondArg.getValue());
+  }
+
+  @SneakyThrows
+  @Test
+  void agreeLicenseActionTest() {
+    machine.getStateMachineAccessor().doWithAllRegions(access -> access
+        .resetStateMachine(new DefaultStateMachineContext<>(INFORMATION_CHANNELS,
+            null, null, extendedState)));
+
+    when(slackService.getUserById(USER_ID)).thenReturn(USER_NAME);
+    when(slackService.sendBlocksMessage(USER_NAME, addGitName)).thenReturn("");
+
+    machine.sendEvent(Event.AGREE_LICENSE);
+
+    verify(slackService).sendBlocksMessage(firstArg.capture(), secondArg.capture());
+    assertEquals(USER_NAME, firstArg.getValue());
+    assertEquals(addGitName, secondArg.getValue());
+  }
+
+  @SneakyThrows
+  @Test
+  void verificationLoginActionTest() {
+    machine.getStateMachineAccessor().doWithAllRegions(access -> access
+        .resetStateMachine(new DefaultStateMachineContext<>(AGREED_LICENSE,
+            null, null, extendedState)));
+
+    when(gitHubService.getUserByLoginInGitHub(USER_NAME)).thenReturn(user);
+    URL url = new URL("http://www.some.com/");
+    when(user.getHtmlUrl()).thenReturn(url);
+    when(slackService.getUserById(USER_ID)).thenReturn(USER_NAME);
+    when(slackService.sendPrivateMessage(USER_NAME, askAboutProfile + "\n" + url)).thenReturn("");
+
+    machine.sendEvent(Event.LOGIN_CONFIRMATION);
+
+    verify(slackService).sendPrivateMessage(firstArg.capture(), secondArg.capture());
+    assertEquals(USER_NAME, firstArg.getValue());
+    assertEquals(askAboutProfile + "\n" + url, secondArg.getValue());
+  }
+
+  @SneakyThrows
+  @Test
+  void addGitNameActionTest() {
+    machine.getStateMachineAccessor().doWithAllRegions(access -> access
+        .resetStateMachine(new DefaultStateMachineContext<>(CHECK_LOGIN,
+            null, null, extendedState)));
+    Set<GHTeam> mockSet = new HashSet<>();
+    mockSet.add(team);
+    when(gitHubService.getUserByLoginInGitHub(USER_NAME)).thenReturn(user);
+    when(gitHubConnectService.getGitHubRepository()).thenReturn(ghRepository);
+    when(ghRepository.getTeams()).thenReturn(mockSet);
+    when(team.getName()).thenReturn("trainees");
+    doNothing().when(team).add(user);
+    when(slackService.getUserById(USER_ID)).thenReturn(USER_NAME);
+    when(slackService.sendPrivateMessage(USER_NAME, congratsAvailableNick)).thenReturn("");
+
+    machine.sendEvent(Event.ADD_GIT_NAME);
+
+    verify(slackService).sendPrivateMessage(firstArg.capture(), secondArg.capture());
+    assertEquals(USER_NAME, firstArg.getValue());
+    assertEquals(congratsAvailableNick, secondArg.getValue());
+  }
+
+  @SneakyThrows
+  @Test
+  void getTheFirstTaskActionTest() {
+    machine.getStateMachineAccessor().doWithAllRegions(access -> access
+        .resetStateMachine(new DefaultStateMachineContext<>(ADDED_GIT,
+            null, null, extendedState)));
+
+    when(slackService.getUserById(USER_ID)).thenReturn(USER_NAME);
+    when(slackService.sendBlocksMessage(USER_NAME, getFirstTask)).thenReturn("");
+
+    machine.sendEvent(Event.GET_THE_FIRST_TASK);
+
+    verify(slackService).sendBlocksMessage(firstArg.capture(), secondArg.capture());
+    assertEquals(USER_NAME, firstArg.getValue());
+    assertEquals(getFirstTask, secondArg.getValue());
+  }
+
+  @SneakyThrows
+  @Test
+  void didNotPassVerificationLoginTest() {
+    machine.getStateMachineAccessor().doWithAllRegions(access -> access
+        .resetStateMachine(new DefaultStateMachineContext<>(CHECK_LOGIN,
+            null, null, extendedState)));
+
+    when(slackService.getUserById(USER_ID)).thenReturn(USER_NAME);
+    when(slackService.sendPrivateMessage(USER_NAME, answeredNoDuringVerification)).thenReturn("");
+
+    machine.sendEvent(Event.DID_NOT_PASS_VERIFICATION_GIT_LOGIN);
+
+    verify(slackService).sendPrivateMessage(firstArg.capture(), secondArg.capture());
+    assertEquals(USER_NAME, firstArg.getValue());
+    assertEquals(answeredNoDuringVerification, secondArg.getValue());
+  }
+
+  @SneakyThrows
+  @Test
+  void checkForNewTaskActionTest() {
+    machine.getStateMachineAccessor().doWithAllRegions(access -> access
+        .resetStateMachine(new DefaultStateMachineContext<>(GOT_THE_FIRST_TASK,
+            null, null, extendedState)));
+    machine.getExtendedState().getVariables()
+        .put("taskNumber", 0);
+
+    String taskMessage =
+        "[{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": "
+            + "\"Here is your next "
+            + "<https://github.com/Broscorp-net/traineeship/tree/master/module1/src/main/java/net/broscorp/"
+            + "checkstyle" + "|TASK>.\"}}]";
+    when(slackService.getUserById(USER_ID)).thenReturn(USER_NAME);
+    when(slackService.sendBlocksMessage(USER_NAME, taskMessage)).thenReturn("");
+
+    machine.sendEvent(Event.GET_THE_NEW_TASK);
+
+    verify(slackService).sendBlocksMessage(firstArg.capture(), secondArg.capture());
+    assertEquals(USER_NAME, firstArg.getValue());
+    assertEquals(taskMessage, secondArg.getValue());
+  }
+
+  @SneakyThrows
+  @Test
+  void changeTaskActionTest() {
+    machine.getStateMachineAccessor().doWithAllRegions(access -> access
+        .resetStateMachine(new DefaultStateMachineContext<>(CHECK_FOR_NEW_TASK,
+            null, null, extendedState)));
+    machine.getExtendedState().getVariables()
+        .put("taskNumber", 0);
+
+    machine.sendEvent(Event.CHANGE_TASK);
+
+    assertEquals(1, machine.getExtendedState().getVariables().get("taskNumber"));
+  }
+
+  @SneakyThrows
+  @Test
+  void lastTaskActionTest() {
+    machine.getStateMachineAccessor().doWithAllRegions(access -> access
+        .resetStateMachine(new DefaultStateMachineContext<>(CHECK_FOR_NEW_TASK,
+            null, null, extendedState)));
+    machine.getExtendedState().getVariables()
+        .put("taskNumber", 15);
+
+    when(slackService.getUserById(USER_ID)).thenReturn(USER_NAME);
+    when(slackService.sendPrivateMessage(USER_NAME, lastTask)).thenReturn("");
+
+    machine.sendEvent(Event.LAST_TASK);
+
+    verify(slackService).sendPrivateMessage(firstArg.capture(), secondArg.capture());
+    assertEquals(USER_NAME, firstArg.getValue());
+    assertEquals(lastTask, secondArg.getValue());
   }
 }
