f1aeac7e60f5f04c8660ceaec3d510473c6ec26e
Hryhorii Perets <grperets@gmail.com>
2021-05-18 23:12:16 +0300

    Added simple sorted

--- src/main/java/com/community/tools/controller/TasksStatusController.java
+++ src/main/java/com/community/tools/controller/TasksStatusController.java
@@ -5,17 +5,25 @@ import com.community.tools.model.User;
 import com.community.tools.service.PullRequestsService;
 import com.community.tools.service.TaskStatusService;
 import com.community.tools.util.statemachie.jpa.StateMachineRepository;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
 import org.kohsuke.github.GHPullRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -40,29 +48,20 @@ public class TasksStatusController {
    * @return webpage with template "tasksstatus"
    */
   @RequestMapping(value = "", method = RequestMethod.GET)
-  public String getTaskStatus(Model model) {
+  public String getTaskStatus(Model model,
+      @RequestParam(name = "sortByField", required = false, defaultValue = "gitName") String sortByField) {
 
     List<UserTasksStatusDto> userTasksStatusDtoList = new ArrayList<>();
-
-    List<User> users = stateMachineRepository.findAll();
+    Page<User> page = stateMachineRepository.findAll(
+        PageRequest.of(0, 100, Sort.by(Sort.Direction.ASC, sortByField)));
+    //List<User> users = stateMachineRepository.findAll();
+    List<User> users = page.getContent();
     users.forEach(user -> {
       userTasksStatusDtoList.add(UserTasksStatusDto.fromUser(user, tasksForUsers));
     });
 
     model.addAttribute("tasksForUsers", tasksForUsers);
     model.addAttribute("userTasksStatuses", userTasksStatusDtoList);
-
-    return "tasksstatus";
-  }
-
-  /**
-   * his method return sorted webpage with table of status.
-   * @param model Model
-   * @param sortedField value for sorted
-   * @return sorted webpage "tasksstatus"
-   */
-  @RequestMapping(value = "/{sortedField}", method = RequestMethod.GET)
-  public String getTaskStatusSorted(Model model, @PathVariable("sortedField") String sortedField) {
 
     return "tasksstatus";
   }
