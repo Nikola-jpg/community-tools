8072592393c56448563e00d5a4c54f45d030090c
Viktor <lebedevit4@gmail.com>
2021-09-01 16:16:12 +0300

    fixed error

--- message-service/src/main/java/com/community/tools/dto/UserDto.java
+++ message-service/src/main/java/com/community/tools/dto/UserDto.java
@@ -3,29 +3,39 @@ package com.community.tools.dto;
 import lombok.Data;
 import net.dv8tion.jda.api.entities.User;
 
+/** javadoc. */
 @Data
 public class UserDto {
-    private String id;
-    private String name;
-    private String displayName;
+  private String id;
+  private String name;
+  private String displayName;
 
+  /**
+   * hello.
+   * @param anotherUser - discordUser.
+   * @return
+   */
+  public static UserDto fromDiscord(User anotherUser) {
+    UserDto user = new UserDto();
 
-    public static UserDto fromDiscord(User anotherUser){
-        UserDto user = new UserDto();
+    user.setId(anotherUser.getId());
+    user.setName(anotherUser.getName());
 
-        user.setId(anotherUser.getId());
-        user.setName(anotherUser.getName());
+    return user;
+  }
 
-        return user;
-    }
-
-    public static UserDto fromSlack(com.github.seratch.jslack.api.model.User anotherUser){
-        UserDto user = new UserDto();
+  /**
+   * hello.
+   * @param anotherUser - slackUser.
+   * @return
+   */
+  public static UserDto fromSlack(com.github.seratch.jslack.api.model.User anotherUser) {
+    UserDto user = new UserDto();
 
     user.setId(anotherUser.getId());
     user.setName(anotherUser.getRealName());
     user.setDisplayName(anotherUser.getProfile().getDisplayName());
 
     return user;
-    }
+  }
 }
