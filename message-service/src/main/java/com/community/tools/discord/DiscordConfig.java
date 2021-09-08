d31c700f3e2ceb2903e4dcd8635da6b200ea18ec
Viktor <lebedevit4@gmail.com>
2021-09-08 14:47:32 +0300

    added profile  into DiscordConfig

--- message-service/src/main/java/com/community/tools/discord/DiscordConfig.java
+++ message-service/src/main/java/com/community/tools/discord/DiscordConfig.java
@@ -16,9 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 
 @Configuration
+@Profile("discord")
 public class DiscordConfig {
 
   @Value("${discord.token}")
