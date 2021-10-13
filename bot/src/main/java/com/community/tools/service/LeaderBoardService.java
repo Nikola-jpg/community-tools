d4e7fcb6276f148ccef01a508464753a10ecdfa7
Danylo Havrylchenko <havrylchenko.d@gmail.com>
2021-10-13 07:51:20 +0300

    Changed project structure, updated index forwarding

--- bot/src/main/java/com/community/tools/service/LeaderBoardService.java
+++ bot/src/main/java/com/community/tools/service/LeaderBoardService.java
@@ -23,8 +23,6 @@ import javax.swing.JEditorPane;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 
 @Service
 public class LeaderBoardService {
@@ -33,53 +31,11 @@ public class LeaderBoardService {
   StateMachineRepository stateMachineRepository;
 
   @Autowired
-  TemplateEngine templateEngine;
-
-  @Autowired
   private MessageService messageService;
 
   @Autowired
   private GitHubService gitHubService;
 
-
-  /**
-   * This method put html code into JEditorPane and print image.
-   * @param url url with endpoint leaderboard
-   * @return byte array with image
-   */
-  @SneakyThrows
-  public byte[] createImage(String url) {
-    String html = getLeaderboardTemplate();
-    int width = 700;
-    int height = 350;
-
-    BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-    Graphics graphics = image.createGraphics();
-
-    JEditorPane jep = new JEditorPane("text/html", html);
-    jep.setSize(width, height);
-    jep.setBackground(Color.WHITE);
-    jep.print(graphics);
-
-    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-    ImageIO.write(image, "png", bos);
-    byte[] data = bos.toByteArray();
-    return data;
-  }
-
-  /**
-   * This method return html-content with table, which contains first 5 trainees of leaderboard.
-   * @return HtmlContent with leaderboard image
-   */
-  public String getLeaderboardTemplate()  {
-    final Context ctx = new Context();
-    List<User> list = getActiveUsersFromPeriod(180);
-    list.sort(Comparator.comparing(User::getTotalPoints).reversed());
-    List<User> listFirst = list.stream().limit(5).collect(Collectors.toList());
-    ctx.setVariable("entities", listFirst);
-    final String htmlContent = this.templateEngine.process("leaderboard.html", ctx);
-    return  htmlContent;
-  }
 
   /**
    * This method load users and add Name to the User model.
