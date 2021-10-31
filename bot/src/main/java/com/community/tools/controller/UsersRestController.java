38a6503a51026ee3b0bb702beb2feefa230f40d8
Danylo Havrylchenko <havrylchenko.d@gmail.com>
2021-10-31 13:13:10 +0200

    Added loader

--- bot/src/main/java/com/community/tools/controller/UsersRestController.java
+++ bot/src/main/java/com/community/tools/controller/UsersRestController.java
@@ -10,6 +10,7 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,6 +38,7 @@ public class UsersRestController {
    * @return returns json with users from db according to query params
    */
   @GetMapping
+  @Transactional
   public List<User> getUsers(@RequestParam(required = false) Integer userLimit,
       @RequestParam(required = false) Integer daysFetch,
       @RequestParam(required = false) String sort) {
